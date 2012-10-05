4f75a9c8d661577831d55936158e544c41274595
DerZyklop <mail@der-zyklop.de>
2012-10-05 03:47:57 +0200

    Revert "Integration of "Hitchwiki-Maps"-Stuff"
    This reverts commit 9f59c595ca57e75e1e3a60860f3dd3137c524a7c.

--- index.php
+++ index.php
@@ -1,8 +1,94 @@
-<?php 
-$site_name = 'index';
-include('inc/templates/head.php');
-?>
+<?php include('inc/php/head.php') ?>
+  <form action="mail.php" method="post">
     <section>
-      <h3>Hi! I'm your index.php, how can i help you?</h3>
+      <h3>Color</h3>
+      <div class="form-elements elements-4">
+        <div class="form-element" id="color-1">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="color-2">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="color-3">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="color-4">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="color-5">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="color-6">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="color-7">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="color-8">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="color-9">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="color-10">
+        </div>
+        <div class="form-element" id="color-11">
+        </div>
+        <div class="form-element" id="color-12">
+        </div>
+<!--        <div class="form-element more-btn">+</div>-->
+        <div class="clearit"></div>
+        <input type="hidden" value="" name="color" id="color" />
+      </div>
     </section>
-<?php include('inc/templates/footer.html') ?>
+
+    <section>
+      <h3>Manufacturer</h3>
+      <div class="form-elements elements-4">
+        <div class="form-element" id="manufacturer-1">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="manufacturer-2">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="manufacturer-3">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="manufacturer-4">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="manufacturer-5">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="manufacturer-6">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="manufacturer-7">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="manufacturer-8">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="manufacturer-9">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="manufacturer-10">
+          <div class="form-element-inner"></div>
+        </div>
+        <div class="form-element" id="manufacturer-11">
+        </div>
+        <div class="form-element" id="manufacturer-12">
+        </div>
+<!--        <div class="form-element more-btn">+</div>-->
+        <div class="clearit"></div>
+        <input type="hidden" value="" name="manufacturer" id="manufacturer" />
+      </div>
+    </section>
+    <section>
+      <h3>Fertig?</h3>
+      <p>
+        <button type="submit">GO!!</button>
+      </p>
+    </section>
+  </form>
+<?php include('inc/php/footer.php') ?>
--- mail.php
+++ mail.php
@@ -1,7 +1,5 @@
-<?php 
-$site_name = 'index';
-include('inc/templates/head.php');
-?>
+<?php include('inc/php/head.php') ?>
+
   <section>
     
     <form action="mail.html"></form>
@@ -68,16 +66,10 @@ include('inc/templates/head.php');
             $manufacturer_name = 'Volvo';
             break;
           case 'manufacturer-8':
-            $manufacturer_name = 'Toyota';
-            break;
-          case 'manufacturer-9':
             $manufacturer_name = 'Smart';
             break;
-          case 'manufacturer-10':
+          case 'manufacturer-9':
             $manufacturer_name = 'Porsche';
-            break;
-          case 'manufacturer-11':
-            $manufacturer_name = 'John Deere tractor';
             break;
         }
         echo( $manufacturer_name );
@@ -123,4 +115,4 @@ This is a generated mail from http://zyklop.pisces.uberspace.de/hitchhiker/ -> A
     </div>
   </section>
 
-<?php include('inc/templates/footer.html') ?>
+<?php include('inc/php/footer.php') ?>
