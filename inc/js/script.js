eff5989c284668889fd8e8236b3ce93d0f74b607
DerZyklop <mail@der-zyklop.de>
2012-10-05 03:49:00 +0200

    Revert "Revert "Integration of "Hitchwiki-Maps"-Stuff""
    This reverts commit 4f75a9c8d661577831d55936158e544c41274595.

--- inc/js/script.js
+++ inc/js/script.js
@@ -9,8 +9,38 @@ jQuery(document).ready(function(){
     var identifier = jQuery(this).attr('id');
     jQuery(this).closest('section').find('input').attr( 'value', identifier );
   });
+/*
   jQuery('.selection .more').click( function () {
     jQuery(this).parent().parent().toggleClass('showall');
     jQuery(this).parent().parent().find('.hidden-items').slideToggle(500);
+  });
+*/
+/*
+  var statusIsActive = false;
+  jQuery('.nav').click( function () {
+    if (statusIsActive) {
+      jQuery(this).find('.other').slideUp();
+      statusIsActive = false;
+    } else {
+      jQuery(this).find('.other').slideDown();
+      statusIsActive = true;
+    }
+    //jQuery(this).parent().parent().find('.hidden-items').slideToggle(500);
+  });
+*/
+  
+  var statusIsActive = false;
+  jQuery('.current').click( function () {
+    if (statusIsActive) {
+      jQuery(this).parent().parent().parent().find('.other').slideUp();
+      statusIsActive = false;
+    } else {
+      jQuery(this).parent().parent().parent().find('.other').slideDown();
+      statusIsActive = true;
+    }
+    jQuery('#bg-overlay').fadeToggle(500);
+  });
+  jQuery('#geo-reload').click( function () {
+    jQuery(this).toggleClass('active');
   });
 });
